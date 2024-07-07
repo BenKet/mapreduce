0c59f3335d2641125214d9086e430fe0d4273adb
BenKet <benedict.kettler@gmail.com>
2024-07-07 15:34:18 +0200

    Update mapper and reducer scripts, add output files

--- mapper.py
+++ mapper.py
@@ -26,6 +26,12 @@ import sys
 # New Line (\n) indicates a new record
 # Output is writing to the standard output
 
+category_sums = {
+    "Computers": 0,
+    "Cameras": 0,
+    "Video Games": 0
+}
+
 # For each new line in the standard input (stdin) 
 for line in sys.stdin:
 
@@ -34,11 +40,18 @@ for line in sys.stdin:
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
 
+    # Check for six elements
+    if len(data) != 6:
+        raise ValueError("Expected 6 elements in the input line, but got {0}: {1}".format(len(data), data))
+
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
 
+    if category in category_sums:
+        category_sums[category] += float(sales)
+
     # Write the key-value combination to standard output (stdout)
-    # Key is the payment, value is the sales     
+    # Key is the payment, value is the sales
     # With a tab (\t) between key and value
     # New line \n means new record
-    sys.stdout.write("{0}\t{1}\n".format(payment, sales))
+    sys.stdout.write("{0}\t{1}\n".format(category, sales))
--- reducer.py
+++ reducer.py
@@ -21,7 +21,7 @@ import sys
 
 # Sum of all sales (values) is initialized with zero, we just started
 sum_of_values = 0
-
+count_of_sales = 0
 # Previous key is initialized with None, we just started
 previous_key = None
 
@@ -41,21 +41,23 @@ for line in sys.stdin:
     # This means the line starts with a new key (key changes e.g. from "Visa" to "Cash")
     # Remember that our keys are sorted
     if previous_key != None and previous_key != key:
-        # Then write the result of the old key (Key=category, Value= Sum of Sales)
-        # to the standart output (stdout)
-        # Key and value are seperated by a tab (\t)
-        # Line ends with new line (\n)
-        sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+        if count_of_values > 0:
+           average_sales = sum_of_sales / count_of_sales
+           # Then write the result of the old key (Key=category, Value= Sum of Sales)
+           # to the standart output (stdout)
+           # Key and value are seperated by a tab (\t)
+           # Line ends with new line (\n)
+           sys.stdout.write("{0}\t{1}\n".format(previous_key, average_sales))
         # Sum of sales starts again with 0
-        sum_of_values = 0
+        sum_of_sales = 0
+        count_of_sales = 0
 
-    # Add the value to the total sales
-    # a += b is the same as a = a + b
-    # the float function transforms the value
-    # to a float data type (like decimal)
-    sum_of_values += float(value)
-    # the previous key for the next iteration is the current key of the this iteration 
+    sum_of_sales += float(value)
+    count_of_sales += 1
+
     previous_key = key
 
 # write the last result to stdout
-sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+if previous_key != None and count_of_sales > 0:
+    average_sales = sum_of_sales / count_of_sales
+    sys.stdout.write("{0}\t{1}\n".format(previous_key, average_sales))
